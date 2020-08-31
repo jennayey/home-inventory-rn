161469ae641a11168cabf9e826c5c6b2e18fe3b2
Jenna Angala <jennayey@gmail.com>
2020-08-31 23:34:13 +0800

    feat: added add item function to item list

--- src/screens/Products/All.js
+++ src/screens/Products/All.js
@@ -2,17 +2,31 @@ import React, {Component, useState} from 'react';
 import {Button, View, Text, FlatList, Modal, TextInput} from 'react-native';
 import ProductEntry from '../../components/Product.Entry';
 import colorSchemes from '../../styles/themes';
+import { set } from 'react-native-reanimated';
 
 function AllProductsScreen() {
   const [items, addItems] = useState([
     {itemName: 'Oreo Cookies', expiryDate: 'December', stock: 3, key: '1'},
     {itemName: 'Cooking Oil', expiryDate: 'November', stock: 1, key: '2'},
-    {itemName: 'Oreo Cookies', expiryDate: 'December', stock: 3, key: '3'},
-    {itemName: 'Oreo Cookies', expiryDate: 'December', stock: 3, key: '4'},
-    {itemName: 'Oreo Cookies', expiryDate: 'December', stock: 3, key: '5'},
+
   ]);
 
-  const [addItemModal, addItemModalVisible] = useState(true);
+  const [addItemModal, addItemModalVisible] = useState(false);
+
+  const [itemName, setItemName] = useState('')
+  const [itemExpiry, setItemExpiry] = useState('')
+  const [itemStock, setItemStock] = useState('')
+
+ 
+  const addProducts = () =>{
+    addItems((prevItems) => {
+     return [
+      {itemName: itemName, expiryDate: itemExpiry, stock: itemStock, key: Math.random().toString()}, 
+      ...items
+    ] 
+    }
+    )
+  }
 
   return (
     <View
@@ -20,7 +34,7 @@ function AllProductsScreen() {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        paddingTop: '5%'
+        paddingTop: '5%',
       }}>
       <Modal
         animationType="slide"
@@ -32,40 +46,50 @@ function AllProductsScreen() {
         <View
           style={{
             // alignItems: 'center',
-          margin: '5%',
+            margin: '5%',
             paddingVertical: '10%',
             paddingHorizontal: '5%',
             backgroundColor: 'white',
             borderRadius: 10,
-            shadowColor: "#000",
+            shadowColor: '#000',
             shadowOffset: {
               width: 50,
-              height: -20
+              height: -20,
             },
             shadowOpacity: 0.5,
             shadowRadius: 3.84,
-            elevation: 10
+            elevation: 10,
           }}>
-          <Text style={{color: colorSchemes().textColor, fontSize: 18, fontWeight: 'bold', marginBottom: 30}} >Add a new item</Text>
+          <Text
+            style={{
+              color: colorSchemes().textColor,
+              fontSize: 18,
+              fontWeight: 'bold',
+              marginBottom: 30,
+            }}>
+            Add a new item
+          </Text>
 
-      <TextInput style={{marginBottom: '1%'}} placeholder='Name of Product' />
-      <TextInput style={{marginBottom: '1%'}} placeholder='No of Stocks' />
-      <TextInput style={{marginBottom: 20}} placeholder='Expiry Date' />
+          <TextInput onChangeText={(text)=> setItemName(text)} style={{marginBottom: '1%'}}  placeholder="Name of Product" />
+          <TextInput onChangeText={(text)=> setItemStock(text)} style={{marginBottom: '1%'}} placeholder="No of Stocks" />
+          <TextInput onChangeText={(text)=> setItemExpiry(text)} style={{marginBottom: 20}} placeholder="Expiry Date" />
 
+          <Button title="Add item" onPress={()=>{addProducts();  addItemModalVisible(!addItemModal)}}></Button>
 
-          <Button
-          
-            title="Add item"
+          <Text
+          style={{margin: 20}}
             onPress={() => {
               addItemModalVisible(!addItemModal);
             }}>
-            {' '}
-          </Button>
+            Cancel
+          </Text>
         </View>
       </Modal>
 
       <Button
-      onPress={()=> {addItemModalVisible(!addItemModal)}}
+        onPress={() => {
+          addItemModalVisible(!addItemModal);
+        }}
         title="Add Item"
         style={{color: colorSchemes().textColor, paddingVertical: 10}}>
         //TO DO: Sort Function here
